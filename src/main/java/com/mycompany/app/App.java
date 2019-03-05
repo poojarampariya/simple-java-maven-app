42e45c58fa6a71e7ce0b26a2098012b2ba5a05a6
poojarampariya <47311489+poojarampariya@users.noreply.github.com>
2019-03-04 21:24:06 -0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Welcome to maven compiling and building tool.....This works awesome.......";
 
     public App() {}
 
